5bc062a94e560fceda3f83d572a794da7b1bfc47
Brian Bolze <brianbolze@gmail.com>
2015-01-28 00:05:23 -0500

    Adding stuff to model

--- EdgeCalApp/models.py
+++ EdgeCalApp/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django_enumfield import enum
 from annoying.fields import AutoOneToOneField
 from django.contrib.auth.models import User
+from codetools.util.cbook import Null
 
 ##############
 # Enumerations
 ##############
 
-class Weekday(enum.Enum):
+class WeekdayEnum(enum.Enum):
     SUNDAY = 0
     MONDAY = 1
     TUESDAY = 2
@@ -16,45 +17,84 @@ class Weekday(enum.Enum):
     FRIDAY = 5
     SATURDAY = 6
     
+class Weekday(models.Model):
+    day = enum.EnumField(WeekdayEnum, default = Null)
+    
 class AccessLevel(enum.Enum):
     PRIVATE = 0
     BUSY = 1
     VISIBLE = 2
     MODIFY = 3
-    
+
+class InvitationStatus(enum.Enum):
+    AWAITING_RESPONSE = 0
+    DECLINED = 1
+    MAYBE = 2
+    ATTENDING = 3
+
+
 ##############
 # Database Entities
 ##############
 
-class CalendarUser(models.Model):
-    # TODO: Add any additional attributes for users
+class CalendarUser(models.Model):    
     user = models.OneToOneField(User)
+    
+    users = models.Manager()
+    
+    # TODO: Add any additional attributes for users
+    
+    def save(self, *args, **kwargs):
+        # do_something()
+        super(CalendarUser, self).save(*args, **kwargs) # Call the "real" save() method.
+        # do_something_else()
+    
+    def __str__(self):
+        return self.commitment_description
+
+# Design Note: Should we rename this something like 'Entry' ?  What if we want to add
+# more general 'tasks' later? I feel like a lot of this could be recycled...
 
 class Event(models.Model):
-    creator = models.ForeignKey(CalendarUser, related_name='creator') # M Events --> 1 Creator
-    start_datetime = models.DateTimeField()
-    end_datetime = models.DateTimeField()
+    creator = models.ForeignKey(CalendarUser, related_name='Creator') # M Events --> 1 Creator
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
     location = models.CharField(max_length=40, blank = True)
-    event_description = models.CharField(max_length=140, blank = True)
+    description = models.CharField(max_length=140, blank = True)
     notes = models.CharField(max_length=500, blank = True)
-    repetition_end_date = models.DateField() 
+    end_repeat_date = models.DateField() 
     public_access_level = enum.EnumField(AccessLevel, default = AccessLevel.PRIVATE)
 
-    members = models.ManyToManyField(CalendarUser, through='IsAttending', through_fields=('event', 'user'))
-    #repetition_scheme = models.ManyToManyField(Weekday, through='EventRepeatsOn', through_fields=('event', 'weekday'))
+    guest_list = models.ManyToManyField(CalendarUser, through='IsInvited', through_fields=('event', 'user'))
+    repetition_scheme = models.ManyToManyField(Weekday)
+    
+    events = models.Manager()
+    
+    class Meta:
+        ordering = ["start_date_time"]
+        verbose_name_plural = "Events"
 
+    # TODO: Specify widgets to be used for each field
     # TODO: Make notes its own class?
+
+    def save(self, *args, **kwargs):
+        # do_something()
+        super(Event, self).save(*args, **kwargs) # Call the "real" save() method.
+        # do_something_else()
 
     def __str__(self):
         return self.commitment_description
     
-class IsAttending(models.Model):
+class IsInvited(models.Model):
     user = models.ForeignKey(CalendarUser)
     event = models.ForeignKey(Event)
+    invitation_status = enum.EnumField(InvitationStatus, default = InvitationStatus.AWAITING_RESPONSE)
+    # invited_by = models.ForeignKey(CalendarUser)
     
-#class EventRepeatsOn(models.Model):
-#    event = models.ForeignKey(Event)
-#    weekday = models.ForeignKey(Weekday)
+    # TODO: Add specific invitation message ?
+    
+    def __str__(self):
+        return self.commitment_description
 
 class HasAccess(models.Model):
     user = models.ForeignKey(CalendarUser)
